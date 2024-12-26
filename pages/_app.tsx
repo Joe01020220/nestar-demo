5624f9c4cef1f5e4166e317d81e8258d269f06ac
Joe01020220 <tursunboyev573@gmail.com>
2024-12-26 13:50:19 +0500

    feat: MUI costimized integration method

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";       // Global integration
+import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
+import { light } from "@/scss/MaterialTheme";
+//import "../scss/app.scss";
+//import "../scss/pc/main.scss";     // Global integration
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // @ts-ignore
+  const[theme, setTheme] = useState(createTheme(light));
+
+  // socket , redux
+  return (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+     <Component {...pageProps} />;   // File tarqatish
+    
+  </ThemeProvider>
+  );
 }
