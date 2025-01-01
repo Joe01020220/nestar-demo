849a597158f357eb79a79412c803670d823c2084
Joe01020220 <tursunboyev573@gmail.com>
2025-01-02 01:56:23 +0500

    feat: publishing  SCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useState } from "react";
 import { light } from "@/scss/MaterialTheme";
-import "../scss/app.scss"
+import "../scss/app.scss";
+import "../scss/pc/main.scss";
   // Global integration
 
 export default function App({ Component, pageProps }: AppProps) {
